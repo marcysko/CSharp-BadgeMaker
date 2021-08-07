2b24d7f3b25e1347868e27988998ce8dab70426a
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-08-07 10:32:48 -0700

    used static populated CSV file with Util

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ while(true)
   Console.Write("Enter last name: ");
   string lastName = Console.ReadLine();
   Console.Write("Enter ID: ");
-int id = Int32.Parse(Console.ReadLine());
+  int id = Int32.Parse(Console.ReadLine());
   Console.Write("Enter Photo URL:");
   string photoUrl = Console.ReadLine();
   Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
@@ -34,18 +34,11 @@ int id = Int32.Parse(Console.ReadLine());
   return employees;
 }
 
-  static void PrintEmployees(List<Employee> employees)
-  {
-    for (int i = 0; i < employees.Count; i++) 
-{
-  string template = "{0,-10}\t{1,-20}\t{2}";
-  Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-}
-  }
 
   static void Main(string[] args) {
   List<Employee> employees = GetEmployees();
-  PrintEmployees(employees);
+  Util.PrintEmployees(employees);
+  Util.MakeCSV(employees);
 }
 }
 }
