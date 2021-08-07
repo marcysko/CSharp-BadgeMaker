e809c3bf2baa8f45210c502a2bd0af89a28de2d4
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-08-06 22:39:41 -0700

    add initial files

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+namespace CatWorx.BadgeMaker {
+  class Employee
+  {
+    private string FirstName;
+    private string LastName;
+    private int Id;
+    private string PhotoUrl;
+    public Employee(string firstName, string lastName, int id, string photoUrl) {
+        FirstName = firstName;
+        LastName = lastName;
+        Id = id;
+        PhotoUrl = photoUrl;
+    }
+    public string GetName() {
+        return  FirstName + " " + LastName;
+    }
+  
+    public int GetId() {
+        return Id;
+    }
+
+    public string GetPhotoUrl() {
+        return PhotoUrl;
+
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+{
+  static List<Employee> GetEmployees()
+{
+List<Employee> employees = new List<Employee>();
+while(true) 
+{
+  // Move the initial prompt inside the loop, so it repeats for each employee
+  Console.WriteLine("Enter first name (leave empty to exit): ");
+
+  // change input to firstName
+  string firstName = Console.ReadLine();
+  if (firstName == "") 
+  {
+    break;
+  }
+
+  // add a Console.ReadLine() for each value
+  Console.Write("Enter last name: ");
+  string lastName = Console.ReadLine();
+  Console.Write("Enter ID: ");
+int id = Int32.Parse(Console.ReadLine());
+  Console.Write("Enter Photo URL:");
+  string photoUrl = Console.ReadLine();
+  Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+  employees.Add(currentEmployee);
+  }
+
+  return employees;
+}
+
+  static void PrintEmployees(List<Employee> employees)
+  {
+    for (int i = 0; i < employees.Count; i++) 
+{
+  string template = "{0,-10}\t{1,-20}\t{2}";
+  Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+}
+  }
+
+  static void Main(string[] args) {
+  List<Employee> employees = GetEmployees();
+  PrintEmployees(employees);
+}
+}
+}
